b78e460773075be979757154a4c20297b87496cd
Jeevendra <jeevendrasingh1999@gmail.com>
2023-09-28 20:57:49 +0530

    Added add todo feature

--- ReactToolKitToDo/src/App.jsx
+++ ReactToolKitToDo/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <h1>This is the tutorial for React tool kit</h1>
+      <AddTodo/>
+      <Todos/>
     </>
   )
 }
--- ReactToolKitToDo/src/components/Todos.jsx
+++ ReactToolKitToDo/src/components/Todos.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import {  useDispatch, useSelector} from "react-redux";
+import { removeTodo } from "../features/todo/todoSlice";
 
 function Todos() {
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
     return (
+        <>
         <div>Todos</div>
+        {
+            todos.map((todo)=>(
+                <li key={todo.id}>{todo.text} <button onClick={()=>{dispatch(removeTodo(todo.id))}}>X</button></li>
+            ))
+        }
+        </>
     )
 }
+export default Todos
