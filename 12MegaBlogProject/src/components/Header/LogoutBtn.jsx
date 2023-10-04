2ea953c10e27bee85ebfdc75bb2b7d411d11428e
Jeevendra <jeevendrasingh1999@gmail.com>
2023-10-05 00:16:04 +0530

    made logout button

---
+++ 12MegaBlogProject/src/components/Header/LogoutBtn.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useDispatch} from 'react-redux'
+import authService from '../../appwrite/auth'
+import {logout} from '../../store/authSlice'
+
+function LogoutBtn() {
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LogoutBtn
