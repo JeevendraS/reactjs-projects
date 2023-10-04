55c57423d1d9bf50cdd5b77c299d90cecbbc84de
Jeevendra <jeevendrasingh1999@gmail.com>
2023-10-04 23:38:01 +0530

    update

--- 12MegaBlogProject/src/App.jsx
+++ 12MegaBlogProject/src/App.jsx
@@ -1,14 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Footer, Header } from './components'
+
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
 
-  return (
-    <>
-      <h1>This is Mega project in Appwrite</h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+  },[])
+
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          Todo:{/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
