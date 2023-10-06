2a36b3e313820273158299ddf86546c5385c33e0
Jeevendra <jeevendrasingh1999@gmail.com>
2023-10-06 21:56:38 +0530

    Container

--- 12MegaBlogProject/src/components/container/Container.jsx
+++ 12MegaBlogProject/src/components/container/Container.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
-function container({children}) {
-  return <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>;
+function Container({children}) {
+  return (
+    <div className='w-full max-w-7xl mx-auto px-4'>{children}</div>
+    )
   
 }
 
-export default container
+export default Container
