70278521f6520576e61f7a268e94015f7f3b6429
Jeevendra <jeevendrasingh1999@gmail.com>
2023-10-06 21:55:07 +0530

    update

--- 12MegaBlogProject/src/components/index.js
+++ 12MegaBlogProject/src/components/index.js
@@ -1,8 +1,15 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import Container from "./container/container";
+import Logo from "./components/Logo"
+import LogoutBtn from "./Header/LogoutBtn";
 
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
