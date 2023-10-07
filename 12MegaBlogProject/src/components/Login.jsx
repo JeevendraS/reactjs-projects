7e98996201811d9d40b44b52438eb2648582a0b7
Jeevendra <jeevendrasingh1999@gmail.com>
2023-10-07 22:26:17 +0530

    update

---
+++ 12MegaBlogProject/src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import { login as authLogin } from '../store/authSlice'
+import {Button, Input, Logo} from './index'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import {useForm} from 'react-hook-form'
+
+
+
+function Login() {
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+    const {register, handleSubmit} = useForm()
+    const [error, setError] = useState()
+
+    const login = async(data)=>{
+        setError("")
+        try {
+            const session = await authService.login(data)
+            if(session){
+                const userData = await authService.getCurrentUser()
+                if(userData) dispatch(authLogin(useData))
+                navigate('/')
+            }
+        } catch (error) {
+            setError(error.message)
+        }
+    }
+  return (
+    <div className='flex items-center justify-center w-full'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+        <div className='mb-2 flex  justify-center w-full '>
+            <span className='inline-block w-full max-w-[100px]'>
+              <Logo width="100%" />  
+            </span>
+
+        </div>
+        <h2 className='text-center text-2xl font-bold leading-tight'>
+            Sign in to your account
+        </h2>
+        <p className='mt-2 text-center text-base text-black/60'>
+            Don&apos;t have any account?&nbsp;
+            <Link 
+            to="/signup"
+            className='font-medium text-primary transition-all duration-200 hover:underline'
+            >
+            Sign Up
+            </Link>
+
+        </p>
+        {error && <p className='text-red-500 text-center'>
+            {error}
+        </p>}
+        <form onSubmit={handleSubmit(login)} className='mt-8'>
+            <div className='space-y-5'>
+                <input 
+                label="Email:"
+                placeholder='Enter you email'
+                type='email'
+                {...register("email")}
+                />
+            </div>
+        </form>
+
+      </div>
+    </div>
+  )
+}
+
+export default Login
