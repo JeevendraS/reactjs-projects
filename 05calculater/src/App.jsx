1e8d65939620bfa26725f4ef5d654b50c2f2fd0c
Jeevendra <jeevendrasingh1999@gmail.com>
2023-11-20 12:11:23 +0530

    indentation correction

--- 05calculater/src/App.jsx
+++ 05calculater/src/App.jsx
@@ -1,53 +1,87 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  let buttons = document.querySelectorAll('.btn');
-  let screenval = document.querySelector('#screen h2')
-  screenval.val = ''
-  let arr = Array.from(buttons)
-  arr.forEach(element => {
-    element.addEventListener("click", (e)=>{
-        let val = e.target.dataset.num;
-        screenval.val += val; 
-    })
-});
+  let buttons = document.querySelectorAll(".btn");
+  let screenval = document.querySelector("#screen h2");
+  screenval.val = "";
+  let arr = Array.from(buttons);
+  arr.forEach((element) => {
+    element.addEventListener("click", (e) => {
+      let val = e.target.dataset.num;
+      screenval.val += val;
+    });
+  });
 
   return (
     <>
-     <div id="main">
+      <div id="main">
         <div id="box">
-          <div id="screen"><h2 >709</h2></div>
+          <div id="screen">
+            <h2>709</h2>
+          </div>
           <div id="buttons">
-            <button type="button" class='btn button' data-num='1'>1</button>
-            <button type="button" class='btn button' data-num='2'>2</button>
-            <button type="button" class='btn button' data-num='3'>3</button>
-            <button type="button" class='btn button' data-num='4'>4</button>
-            <button type="button" class='btn button' data-num='5'>5</button>
-            <button type="button" class='btn button' data-num='6'>6</button>
-            <button type="button" class='btn button' data-num='7'>7</button>
-            <button type="button" class='btn button' data-num='8'>8</button>
-            <button type="button" class='btn button' data-num='9'>9</button>
-            <button type="button" class='btn button' data-num='0'>0</button>
-            <button type="button" class='btn button' data-num='.'>.</button>
+            <button type="button" class="btn button" data-num="1">
+              1
+            </button>
+            <button type="button" class="btn button" data-num="2">
+              2
+            </button>
+            <button type="button" class="btn button" data-num="3">
+              3
+            </button>
+            <button type="button" class="btn button" data-num="4">
+              4
+            </button>
+            <button type="button" class="btn button" data-num="5">
+              5
+            </button>
+            <button type="button" class="btn button" data-num="6">
+              6
+            </button>
+            <button type="button" class="btn button" data-num="7">
+              7
+            </button>
+            <button type="button" class="btn button" data-num="8">
+              8
+            </button>
+            <button type="button" class="btn button" data-num="9">
+              9
+            </button>
+            <button type="button" class="btn button" data-num="0">
+              0
+            </button>
+            <button type="button" class="btn button" data-num=".">
+              .
+            </button>
 
-            <button type="button" class='btn button orange' data-num='+'>+</button>
-            <button type="button" class='btn button orange' data-num='-'>-</button>
-            <button type="button" class='btn button orange' data-num='*'>*</button>
-            <button type="button" class='btn button orange' data-num='/'>/</button>
+            <button type="button" class="btn button orange" data-num="+">
+              +
+            </button>
+            <button type="button" class="btn button orange" data-num="-">
+              -
+            </button>
+            <button type="button" class="btn button orange" data-num="*">
+              *
+            </button>
+            <button type="button" class="btn button orange" data-num="/">
+              /
+            </button>
 
-            <button type="button" class='button yellow'>C</button>
-            <button type="button" class='button red'>DEL</button>
-            <button type="button" class='button green'>=</button>
-
-
-
-
+            <button type="button" class="button yellow">
+              C
+            </button>
+            <button type="button" class="button red">
+              DEL
+            </button>
+            <button type="button" class="button green">
+              =
+            </button>
           </div>
         </div>
-     </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
