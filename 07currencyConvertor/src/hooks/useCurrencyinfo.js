d1cadcc0f035bdf1a4207e2d71df1183a8cf8755
Jeevendra <jeevendrasingh1999@gmail.com>
2023-12-03 12:30:07 +0530

    indentation correction

--- 07currencyConvertor/src/hooks/useCurrencyinfo.js
+++ 07currencyConvertor/src/hooks/useCurrencyinfo.js
@@ -1,14 +1,16 @@
-import { useState, useEffect} from 'react'
+import { useState, useEffect } from "react";
 
 function useCurrencyInfo(currency) {
-    const [data, setData] = useState({})
-    useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then((res)=> res.json())
-        .then((res) => setData(res[currency]))
-    },[currency])
-    console.log(data)
-    return data
+  const [data, setData] = useState({});
+  useEffect(() => {
+    fetch(
+      `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res[currency]));
+  }, [currency]);
+  console.log(data);
+  return data;
 }
 
 export default useCurrencyInfo;
