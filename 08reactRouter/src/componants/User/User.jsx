d21d44f15788be44604f0e0a62d795cd1cf59310
Jeevendra <jeevendrasingh1999@gmail.com>
2023-12-16 15:30:46 +0530

    indentation update

--- 08reactRouter/src/componants/User/User.jsx
+++ 08reactRouter/src/componants/User/User.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React from "react";
+import { useParams } from "react-router-dom";
 
 function User() {
-    const {UserId} = useParams()
+  const { UserId } = useParams();
   return (
-    <div className='bg-gray-500 text-center text-white text-4xl p-4'>User:{UserId}</div>
-  )
+    <div className="bg-gray-500 text-center text-white text-4xl p-4">
+      User:{UserId}
+    </div>
+  );
 }
 
-export default User
+export default User;
