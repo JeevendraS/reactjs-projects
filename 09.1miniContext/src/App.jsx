b00ea5da3ba6afedf140445969a065d2d20064db
Jeevendra <jeevendrasingh1999@gmail.com>
2023-12-28 15:42:03 +0530

    indentation update

--- 09.1miniContext/src/App.jsx
+++ 09.1miniContext/src/App.jsx
@@ -1,17 +1,16 @@
-import './App.css'
-import Login from './componant/Login'
-import Profile from './componant/Profile'
-import UserContextProvider from './context/UserconstextProvider'
+import "./App.css";
+import Login from "./componant/Login";
+import Profile from "./componant/Profile";
+import UserContextProvider from "./context/UserconstextProvider";
 
 function App() {
-
   return (
     <UserContextProvider>
-     <h1>Hello my name is Jeevendra singh</h1>
-     <Login/>
-     <Profile/>
-    </UserContextProvider >
-  )
+      <h1>Hello my name is Jeevendra singh</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  );
 }
 
-export default App
+export default App;
